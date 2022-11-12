72a003e730a1cd95e50093bc54dbb7566417a6e8
Ahsan Khan <mkhan201122@bscse.uiu.ac.bd>
2022-11-12 23:17:59 +0600

    Update A_Star_Search.py

--- Assignment-1/A_Star_Search.py
+++ Assignment-1/A_Star_Search.py
@@ -21,14 +21,22 @@ class Node:
     def add_adjNode(self, node):
         self.adjNode.append(node)
 
+    def make_copy(self):
+        n = Node(self.name, self.x, self.y)
+        n.adjNode = self.adjNode
+        return n
+
+    def make_copy_fresh(self):
+        return Node(self.name, self.x, self.y)
+
     def euclidean(self, d2):
         return math.sqrt((self.x - d2.x)**2 + (self.y - d2.y)**2)
 
     def __lt__(self, o):
-        return self.cost < o.cost
+        return self.f < o.f
 
     def __eq__(self, o):
-        return self.name == o.name and self.x == o.x and self.y == o.y and self.cost == o.cost
+        return self.f == o.f
 
     def __str__(self):
         return f'{self.name} {self.x} {self.y}'
@@ -44,57 +52,51 @@ class Graph:
     def add_edge(self, u, v, cost):
         node_u = self.nodes[u]
         node_v = self.nodes[v]
-        node_u.update_cost(cost)
         node_v.update_cost(cost)
         node_u.add_adjNode(node_v)
-        # node_v.add_adjNode(node_u)
 
     # A* Search 
     def a_star(self, start, goal):
         for node in self.nodes.values():
-            node.f = node.euclidean(goal)
+            node.h = node.euclidean(goal)
 
-        start_node = self.nodes[start.name]
-        goal_node = self.nodes[goal.name]
-        start_node.h = start_node.euclidean(goal_node)
-        start_node.f = start_node.h
-        queue = PriorityQueue()
-        queue.put(start_node)
-        while not queue.empty():
-            node = queue.get()
-            if node.name == goal_node.name:
-                self.path.append(node)
-                while node.parent is not None:
-                    self.path.append(node.parent)
-                    node = node.parent
-                return self.path
-            for adj in node.adjNode:
-                print(node, end=' -> ')
-                if adj.name == start_node.name or adj.name == node.name:
-                    continue
-                adj.h = adj.euclidean(goal_node)
-                adj.f = node.cost + adj.h
-                adj.update_parent(node)
-                queue.put(adj)
-                print(adj)
-        return None
+        q = PriorityQueue()
+        start.f = start.h
+        start.cost = 0
+        start.update_parent(None)
+        q.put(start)
 
+        while not q.empty():
+            curr = q.get()
+            if curr.name == goal.name:
+                while curr is not None:
+                    self.path.append(curr)
+                    curr = curr.parent
+                return True
+
+            # print(f'curr: {curr.name} {curr.f} -> ', end='')
+            for neighbor in curr.adjNode:
+                nn = neighbor.make_copy()
+                nn.update_parent(curr)
+                nn.update_cost(curr.cost + neighbor.cost)
+                nn.f = nn.cost + nn.h
+                q.put(nn)
+                # print(nn.name, nn.f)
+        return False
 
     def print_path(self):
         n = len(self.path)
+        print('Solution path', end=' ')
         for i in range(n):
             node = self.path.pop()
             print(node.name, end='')
             if i != n-1:
                 print('-', end='')
-        print()
-
-    def get_cost(self):
-        return self.path[-1].cost
+        print('\nSolution cost', node.cost)
 
     def print_graph(self):
         for node in self.nodes.values():
-            print(node, end=' -> ')
+            print(node.name, end=' -> ')
             for adj in node.adjNode:
                 print(adj.name, end=' ')
             print()
@@ -108,11 +110,11 @@ if __name__ == '__main__':
     f = open('input.txt')
     n = int(f.readline())
     for i in range(n):
-        name, x, y = f.readline().split()
+        name, x, y = f.readline().strip().split()
         graph.add_node(Node(name, int(x), int(y)))
     n = int(f.readline())
     for i in range(n):
-        u, v, cost = f.readline().split()
+        u, v, cost = f.readline().strip().split()
         graph.add_edge(u, v, int(cost))
 
 
@@ -123,9 +125,15 @@ if __name__ == '__main__':
     start = graph.nodes[start]
     goal = graph.nodes[goal]
 
-    graph.a_star(start, goal)
     # graph.print_graph()
-    graph.print_path()
+    result = graph.a_star(start, goal)
+    if result:
+        graph.print_path()
+    else:
+        print('No path found')
+    
+    # graph.print_graph()
+    # graph.print_path()
     # g = graph.nodes[goal.name]
     # while g.name != goal.name:
     #     print(goal.name, end=' -> ')
@@ -139,7 +147,6 @@ if __name__ == '__main__':
 
     # output
     # if found:
-    #     print('No path')
     # else:
     #     print('Solution path', end=' ')
     #     graph.print_path()
